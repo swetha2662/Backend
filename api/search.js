d6e1f349acc578ac318b8b75d9a6635821c40195
Swetha S <85669283+swetha2662@users.noreply.github.com>
2023-02-28 12:07:06 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 3000
 const supabase = createClient(
     'https://cfvrhdvuvazkvqftofkp.supabase.co',
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmdnJoZHZ1dmF6a3ZxZnRvZmtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1NjIzNTYsImV4cCI6MTk5MzEzODM1Nn0.hclCd3UBhok4wtNXj2ybJlTPNBSmxgWME7UyoedOuM8'
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmdnJoZHZ1dmF6a3ZxZnRvZmtwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NzU2MjM1NiwiZXhwIjoxOTkzMTM4MzU2fQ.JlHeYII8n1a9VdImtvbAvmjjgVy-fPO6a-_10d1dxvc'
 );
 app.use(bodyparse.json())
 app.use(
